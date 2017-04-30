69db46ca4faf16cf061247c9df359849d8a5ba8a
D Rebecca Haliburton <haliburtonre@gmail.com>
2017-04-30 12:02:35 -0700

    Added new pen styiling button, customized grid size and optimized pen size.

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
-  for (var x = 0; x < 16; x++) {
-    for (var y = 0; y < 16; y++) {
-    $("<div class='square'></div>").appendTo("#container");
+  for (var x = 0; x < 100; x++) {
+    for (var y = 0; y < 100; y++) {
+    var $square = $("<div class='square'></div>");
+    $square.appendTo("#container");
     }
   }
   $("div.square").hover(function() {
     $(this).css("background-color", "gray");
   });
-  $("button").text("Clear");
-  $("button").on("click", function() {
+  $("#clear").text("Clear");
+  $("#clear").on("click", function() {
     $("div.square").css("background-color", "none");
   });
+  $("#gridsize").text("Grid Size");
+  $("#gridsize").on("click", function() {
+    var gridinput = prompt("What grid size? (100-1000)");
+    $("#container").css("height", gridinput);
+    $("#container").css("width", gridinput);
+    $("div.square").css("background-color", "none");
+    $("div.square").hover(function() {
+      $(this).css("background-color", "gray");
+    });
+  });
+  $("#pencolour").text("Pen Colour");
+  $("#pencolour").on("click", function() {
+    var colourinput = prompt("What pen colour? (eg. red, blue, #666666)");
+    $("div.square").hover(function() {
+      $(this).css("background-color", colourinput);
+    });
+  });
+
 });
